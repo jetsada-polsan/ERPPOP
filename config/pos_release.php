66ad1e75a8605cd4b52940675338d7177022c27d
jetsada-polsan <jetsadapolsanjeje@gmail.com>
2026-07-29 19:32:47 +0700

    fix: keep ERP blue and correct POS release asset URL

--- config/pos_release.php
+++ config/pos_release.php
@@ -7,7 +7,7 @@ return [
     'platforms' => [
         'windows-x86_64' => [
             'signature' => 'dW50cnVzdGVkIGNvbW1lbnQ6IHNpZ25hdHVyZSBmcm9tIHRhdXJpIHNlY3JldCBrZXkKUlVTZmNtY0VocGZJR0dVV2dxbld4c3RqTkxsdWxJcVhVbTBVUzhDeTh6TGI4NGYwTWVUbEJMcGhUMm4rK1dlaHpBcjFta3cwdzRpcldSeHJOR1lTeEI2RmEyYUMzOG9sOWdFPQp0cnVzdGVkIGNvbW1lbnQ6IHRpbWVzdGFtcDoxNzg1MzEzNzM2CWZpbGU6UE9QU1RBUiBQT1NfMS40LjFfeDY0LXNldHVwLmV4ZQo2Qkx2dDlXNlFzM3N4QTgxajdSSE9YdlZIWFNZcHNvYnJnekVsb2VTbjVac0h2Q28ya2IzR2h4dVJ3QWZzekZ3eU1oRTA1Y2tqbzExcmtiV2pCQT09Cg==',
-            'url' => 'https://github.com/jetsada-polsan/ERPPOP/releases/download/v1.4.1/POPSTAR%20POS_1.4.1_x64-setup.exe',
+            'url' => 'https://github.com/jetsada-polsan/ERPPOP/releases/download/v1.4.1/POPSTAR.POS_1.4.1_x64-setup.exe',
         ],
     ],
     'mandatory' => false,
